5450483af19020f56ffc5d1859e102aeac804857
liusx <imliusx@163.com>
2026-06-30 05:35:25 +0800

    style: reduce sidebar logo icon size

--- src/components/layout/team-switcher.tsx
+++ src/components/layout/team-switcher.tsx
@@ -28,7 +28,7 @@ export function TeamSwitcher({ teams }: TeamSwitcherProps) {
           className="flex h-11 w-full select-none items-center gap-1.5 overflow-hidden rounded-lg p-0 text-sidebar-foreground outline-hidden ring-sidebar-ring transition-colors hover:text-sidebar-accent-foreground focus-visible:ring-2 group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:justify-center"
         >
           <div className="flex aspect-square size-8 shrink-0 items-center justify-center text-primary">
-            <activeTeam.logo className="!size-8" />
+            <activeTeam.logo className="!size-7" />
           </div>
           <div className="grid flex-1 text-start text-lg leading-tight group-data-[collapsible=icon]:hidden">
             <span className="truncate font-semibold">{activeTeam.name}</span>
